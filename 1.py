a389f6d0f11369b954c013c4632f87bb473ca890
NathanENc <nathanelian11@gmail.com>
2024-11-20 00:21:36 -0600

    Resolvimos conflictos

---
+++ 1.py
@@ -0,0 +1,4 @@
+#Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
+nombre = str(input("¿Cuál es tu nombre: "))
+print("Hola", nombre, "!")
+#No ocupar str ya que input devuelve una cadena de texto
