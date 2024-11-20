e0acc562501aeaf06f89e015e8cf438d498f60df
NathanENc <nathanelian11@gmail.com>
2024-11-20 01:12:05 -0600

    Añadí el ejercicio de operación aritmética

---
+++ 02_operacion_aritmetica.py
@@ -0,0 +1,5 @@
+#Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética ((3+2)/2*5)**2
+
+#Escibir operación
+resultado  = (((3 + 2) / (2 * 5)) ** 2)
+print("Se resolverá la siguiente operación: ", resultado )
